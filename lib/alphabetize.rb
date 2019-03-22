ccfc21f7fe9b718f4308faae70834ebe0fd16418
Jeff M <jkdmarks@gmail.com>
2019-03-22 17:37:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,20 +1,20 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-puts ESPERANTO_ALPHABET.index('m')
 
 def alphabetize(arr)
   
   arr.sort do |word1, word2|
     shorter_word = word1 < word2 ? word1 : word2
     
-    diff_letter = 0
-    for i in (0...shorter_word.length)
-      until word1[i] != word2[i]
-        diff_letter += 1
-        puts word1[diff_letter]
-        puts word2[diff_letter]
-      end
+    i = 0
+    until word1[i] != word2[i]
+      i += 1
+      puts word1[i]
+      puts word2[i]
     end
     
+    word1[i] <=> word2[i]
   end
+
+  arr
 end
